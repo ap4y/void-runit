3a854bda5ef00fb71422b952c1e775b7f938e0ef
ap4y <mail@ap4y.me>
2020-09-30 21:54:59 +1300

    Support pure v2 cgroup mounts
    Some of the tooling that rely on cgroups (notably podman, runc and
    crun) enable cgroup2 mode only if /sys/fs/cgroup has a v2 magic
    number. This commit introduces configuration option that controls the
    way cgroup is mounted, 3 modes are supported:
    - hybrid: current mode with v1 and v2 mounted
    - unified: v2 only mode
    - legacy: v1 only mode
    
    This is modeled after OpenRC:
    https://github.com/OpenRC/openrc/blob/72df51e17ba0e1a0f94451b4bbfb338288c4625c/init.d/cgroups.in#L121-L129

--- core-services/00-pseudofs.sh
+++ core-services/00-pseudofs.sh
@@ -1,5 +1,23 @@
 # vim: set ts=4 sw=4 et:
 
+mount_cgroup1()
+{
+    mountpoint -q /sys/fs/cgroup || mount -o mode=0755 -t tmpfs cgroup /sys/fs/cgroup
+    awk '$4 == 1 { system("mountpoint -q /sys/fs/cgroup/" $1 " || { mkdir -p /sys/fs/cgroup/" $1 " && mount -t cgroup -o " $1 " cgroup /sys/fs/cgroup/" $1 " ;}" ) }' /proc/cgroups
+}
+
+mount_cgroup2()
+{
+    local path
+    case "${CGROUP_MODE:-hybrid}" in
+        hybrid) path="/sys/fs/cgroup/unified" ;;
+        unified) path="/sys/fs/cgroup" ;;
+    esac
+    echo $path
+    mkdir -p "${path}"
+    mountpoint -q "${path}" || mount -t cgroup2 -o nsdelegate cgroup2 "${path}"
+}
+
 msg "Mounting pseudo-filesystems..."
 mountpoint -q /proc || mount -o nosuid,noexec,nodev -t proc proc /proc
 mountpoint -q /sys || mount -o nosuid,noexec,nodev -t sysfs sys /sys
@@ -11,10 +29,12 @@ mountpoint -q /dev/shm || mount -o mode=1777,nosuid,nodev -n -t tmpfs shm /dev/s
 mountpoint -q /sys/kernel/security || mount -n -t securityfs securityfs /sys/kernel/security
 
 if [ -z "$VIRTUALIZATION" ]; then
-    # cgroup v1
-    mountpoint -q /sys/fs/cgroup || mount -o mode=0755 -t tmpfs cgroup /sys/fs/cgroup
-    awk '$4 == 1 { system("mountpoint -q /sys/fs/cgroup/" $1 " || { mkdir -p /sys/fs/cgroup/" $1 " && mount -t cgroup -o " $1 " cgroup /sys/fs/cgroup/" $1 " ;}" ) }' /proc/cgroups
-    # cgroup v2
-    mkdir -p /sys/fs/cgroup/unified
-    mountpoint -q /sys/fs/cgroup/unified || mount -t cgroup2 -o nsdelegate cgroup2 /sys/fs/cgroup/unified
+    case "${CGROUP_MODE:-hybrid}" in
+        hybrid)
+            mount_cgroup1
+            mount_cgroup2
+            ;;
+        legacy) mount_cgroup1 ;;
+        unified) mount_cgroup2 ;;
+    esac
 fi
